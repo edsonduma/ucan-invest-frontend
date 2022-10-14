a916144e65f45c905d8e4b0c84c0160066435b1a
Hecroesmo <hecroesmo.jesus77@outlook.com>
2022-10-14 11:10:05 +0100

    fixed exposed bearer token

--- api/academic-levels.js
+++ api/academic-levels.js
@@ -5,7 +5,7 @@ export async function findAllAcademicLevel() {
   console.log('findAllAcademicLevel', getCookieFromBrowser('token'))
   return axios.get(`${process.env.NEXT_PUBLIC_BASE_URI}/academicLevels`, {
     headers: {
-      'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY2NjMzODM3MiwiaWF0IjoxNjY1NzMzNTcyfQ.lAY1_599j6vI7FkAxr7lWotjHWFh_YPG9VxOqiFTy9-sKj_clzBefyZY-qc_-TTL7hImbmwFBCpH4Ca4kailjg'
+      'Authorization': getCookieFromBrowser('token')
     }
   })
 }
